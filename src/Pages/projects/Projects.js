5bf852a6f97869a756671ca11ee987153d232876
Ahedabushahen <ahed10ah@gmail.com>
2025-05-16 18:55:33 +0300

    fix import path for Projects.css

--- src/Pages/projects/Projects.js
+++ src/Pages/projects/Projects.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import '../projects/Projects.css'; 
+import './Projects.css'; // Import your CSS file for styling
 
 const Projects = () => {
   const [projects, setProjects] = useState([]);
