1c37977400e6d58cdd728bcdfea49c656eb7110f
Ahedabushahen <ahed10ah@gmail.com>
2025-05-16 18:59:31 +0300

    refactor: clean up import statement for Projects.css

--- src/Pages/projects/Projects.js
+++ src/Pages/projects/Projects.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './Projects.css'; // Import your CSS file for styling
+import './Projects.css'; 
 
 const Projects = () => {
   const [projects, setProjects] = useState([]);
